651ba1d11922762e14deb29b41784824d5df1d78
SantoshSah <santoshsah4@gmail.com>
2016-09-17 19:19:38 +0545

    added

---
+++ app/shared/flow-config.provider.ts
@@ -0,0 +1,11 @@
+import { OpaqueToken } from '@angular/core';
+
+//set flowjs configuration
+
+const FLOWJS_CONFIG = {
+  target:'/'
+};
+
+export let FLOWJSCONFIG = new OpaqueToken(FLOWJS_CONFIG);
+
+
